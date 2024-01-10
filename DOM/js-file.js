f16c1800471f7f0c455d12ffd22e2956ec87c71c
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-01-10 08:05:58 +0200

    added h3

--- DOM/js-file.js
+++ DOM/js-file.js
@@ -8,3 +8,8 @@ p.style.color = 'red';
 content.classList.add('p');
 p.textContent = "hey i'm red!";
 container.appendChild(p);
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+content.classList.add('h3');
+h3.textContent = "i'm a blue h3";
+container.appendChild(h3);
